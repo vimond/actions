1c9dde3e23a4743e37661ad9906e6ace8201ad57
Erlend Fonnes <erlend@vimond.com>
2022-07-11 15:27:18 +0200

    fix: fixed pushing items to dynamodb

--- ticket-storer/index.js
+++ ticket-storer/index.js
@@ -1,33 +1,93 @@
 require('dotenv').config();
 const ticketSender = require('./src/dynamodb');
 const core = require('@actions/core');
+const ticketFinder = require('../ticket-collector/src/tickets-finder');
+const jira = require('../ticket-collector/src/jira');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    console.log("Start storing tickets")
+    console.log("Start storing tickets");
 
     const input = {
       owner: core.getInput("owner", { required: true }),
       repo: core.getInput("repository", { required: true }),
-      commitSha: core.getInput("commit-sha", { required: true }),
-      parentSha: core.getInput("parent-sha", { required: true }),
-      tag: core.getInput("tag"),
-      branch: core.getInput("branch", { required: true }),
-      tickets: core.getInput("tickets"),
-      createdAt: core.getInput("created-at", { required: true })
+      refName: core.getInput("ref-name", { required: true }),
+      refType: core.getInput("ref-type", { required: true }),
+      refBase: core.getInput("ref-base", { required: true }),
+      previousSha: core.getInput("previousSha", { required: true }),
+      newSha: core.getInput("newSha", { required: true }),
+      commits: core.getInput("commits", { required: true })
     };
 
     const awsConfig = {
-      creds: core.getInput("aws-credentials", { required: true }),
-      region: core.getInput("aws-region", { required: true }),
       tableName: core.getInput("dynamodb-tablename", { required: true }),
     };
 
-    // Hide secrets
-    core.setSecret(awsConfig.creds);
+    const jiraConfig = {
+      host: core.getInput("jira-host"),
+      proxy: core.getInput("jira-proxy"),
+      username: core.getInput("jira-username"),
+      token: core.getInput("jira-token")
+    };
 
-    const resp = await ticketSender.send(input, awsConfig);
+    // Hide secrets
+    core.setSecret(jiraConfig.token);
+    core.setSecret(jiraConfig.username);
+
+
+    if (input.refType === "tag") {
+      let branch = "";
+      if (input.refBase.startsWith("refs/heads/")) {
+          branch = input.refBase.slice("refs/heads/".length);
+      }
+
+      const resp = await ticketSender.storeTag(awsConfig, {
+          ownerRepo: input.owner + ":" + input.repo,
+          tag: input.refName,
+          commitSha: input.newSha,
+          branch: branch
+      });
+      console.log(`Tickets sent: ${JSON.stringify(resp)}`);
+      return;
+    }
+
+    let commitTickets = {};
+    let allTickets = [];
+    let validTickets = {};
+    for (const commit of input.commits) {
+        const tickets = ticketFinder.findAll([commit.message]);
+        commitTickets[commit.id] = tickets;
+        allTickets = allTickets.concat([...tickets]);
+    }
+
+    const filteredTickets = jira.checkIfExist(jiraConfig, new Set(allTickets));
+    for (const ticket of filteredTickets) {
+        validTickets[ticket] = true;
+    }
+
+    let commits = [];
+    let parentSha = input.previousSha;
+    for (const commit of input.commits) {
+        let tickets = [];
+        for (const ticket of commitTickets[commit.id]) {
+            if (ticket in validTickets) {
+                tickets.push(ticket);
+            }
+        }
+
+        commits.push({
+            ownerRepo: input.owner + ":" + input.repo,
+            commitSha: commit.id,
+            parentSha: parentSha,
+            branch: input.refName,
+            tickets: tickets
+        });
+        parentSha = commit.id;
+    }
+
+
+    const resp = await ticketSender.storeCommits(awsConfig, commits);
     console.log(`Tickets sent: ${JSON.stringify(resp)}`);
   } catch (error) {
     core.setFailed(error.message);
--- ticket-storer/src/dynamodb.js
+++ ticket-storer/src/dynamodb.js
@@ -1,22 +1,62 @@
-const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBClient, PutItemCommand, BatchWriteItemCommand } = require("@aws-sdk/client-dynamodb");
 
-async function send(input, awsConfig) {
-    const client = new DynamoDBClient({ region: awsConfig.region })
+const maxItemsNum = 25;
+
+async function storeCommits(awsConfig, commits) {
+    const client = new DynamoDBClient();
+
+    let resp = [];
+    for (let i = 0; i < commits.length; i += maxItemsNum) {
+        resp.push(await client.send(new BatchWriteItemCommand({
+            RequestItems: {
+                [awsConfig.tableName]: commits.slice(i, i + maxItemsNum).map(function (item) {
+                    let obj = {
+                        "OwnerRepo": { S: item.ownerRepo },
+                        "ID": { S: `commit:${item.commitSha}` },
+                        "ParentSha": { S: item.parentSha }
+                    };
+
+                    if (item.Branch != "" && item.branch !== undefined) {
+                        obj["Branch"] = { S: item.branch };
+                    }
+
+                    if (item.tickets.length > 0) {
+                        obj["Tickets"] = { SS: item.tickets };
+                    }
+
+                    return {
+                        PutRequest: {
+                            Item: obj
+                        }
+                    };
+                })
+            }
+        })));
+    }
+
+    return resp;
+}
+
+async function storeTag(awsConfig, tagInfo) {
+    const client = new DynamoDBClient();
+
+    let item = {
+        "OwnerRepo": { S: tagInfo.ownerRepo },
+        "ID": { S: `tag:${tagInfo.tag}` },
+        "MappedCommit": { S: tagInfo.commitSha }
+    };
+
+    if (tagInfo.branch !== "" && tagInfo.branch !== undefined) {
+        item["Branch"] = { S: tagInfo.branch };
+    }
 
     return await client.send(new PutItemCommand({
-        Item: {
-            "PK": { S: input.owner + ":" + input.repo },
-            "CommitSha": { S: input.commitSha },
-            "ParentSha": { S: input.parentSHA },
-            "Tag": { S: input.tag },
-            "Branch": { S: input.branch },
-            "Ticekts": { SS: input.tickets },
-            "Timestamp": { S: input.createdAt },
-        },
+        Item: item,
         TableName: awsConfig.tableName
     }));
 }
 
 module.exports = {
-    send
+    storeCommits,
+    storeTag
 }
