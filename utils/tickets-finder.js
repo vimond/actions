3c23de2fa2876eafedb210aad081d06514079721
Dag Viggo Lokøen <dagviggo@vimond.com>
2022-10-07 13:56:50 -0700

    debug

--- utils/tickets-finder.js
+++ utils/tickets-finder.js
@@ -1,8 +1,11 @@
 const ticketRegex = /[A-Z]{2,}-\d+/gm;
 
 function findAll(textBlocks) {
+    console.log("findAll");
+    console.log(textBlocks);
     let ticketsFound = new Set();
     for ( const text of textBlocks) {
+        console.log(text);
         const matches = [...text.matchAll(ticketRegex)];
         matches.forEach( m => {
             ticketsFound.add(m[0]);
