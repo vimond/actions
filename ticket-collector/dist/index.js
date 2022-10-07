33b9f79a997e44f3e088e37077e824a55e03bebe
Dag Viggo Lokøen <dagviggo@vimond.com>
2022-10-07 14:00:08 -0700

    debug

--- ticket-collector/dist/index.js
+++ ticket-collector/dist/index.js
@@ -9757,8 +9757,11 @@ module.exports = {
 const ticketRegex = /[A-Z]{2,}-\d+/gm;
 
 function findAll(textBlocks) {
+    console.log("findAll");
+    console.log(textBlocks);
     let ticketsFound = new Set();
     for ( const text of textBlocks) {
+        console.log(text);
         const matches = [...text.matchAll(ticketRegex)];
         matches.forEach( m => {
             ticketsFound.add(m[0]);
@@ -9770,6 +9773,7 @@ function findAll(textBlocks) {
 module.exports = {
     findAll
 }
+
 
 /***/ }),
 
